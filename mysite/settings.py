c3a5e6c7eb4d692043d464e0ff778b6eadd7a84a
vladlipski <vladislav.lipsky@gmail.com>
2017-01-23 17:31:49 +0300

    django-allauth added

--- mysite/settings.py
+++ mysite/settings.py
@@ -17,6 +17,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.vk',
     'polls',
 ]
 
@@ -68,9 +73,6 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -105,3 +107,15 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = '/static/'
+
+
+#django-allauth
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID = 1
--- polls/models.py
+++ polls/models.py
@@ -2,9 +2,12 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    voted_users = models.ManyToManyField(User, related_name="voted_questions", blank=True, default=None)
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
@@ -21,4 +24,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_text
+        return self.choice_textpy
